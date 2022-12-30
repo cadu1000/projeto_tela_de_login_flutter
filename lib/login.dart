59dcb2c469046d7088a73a8e5b71fd6b41a9241c
eduardo <cezamperlini@gmail.com>
2022-12-30 16:40:41 -0300

    first commit

---
+++ lib/login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AppLogin extends StatefulWidget {
+  const AppLogin({super.key});
+
+  @override
+  State<AppLogin> createState() => _AppLoginState();
+}
+
+class _AppLoginState extends State<AppLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
