cb7a08809a564361fd52240f185f42943ef3d792
eduardo <cezamperlini@gmail.com>
2023-01-06 20:30:53 -0300

    tela de login com email e senha preliminar

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class AppLogin extends StatefulWidget {
   const AppLogin({super.key});
@@ -9,8 +8,330 @@ class AppLogin extends StatefulWidget {
 }
 
 class _AppLoginState extends State<AppLogin> {
+  bool _isChecked = true;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+        color: const Color.fromARGB(255, 4, 37, 44),
+        //
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 7,
+                ),
+                child: Image.asset(
+                  "assets/img/earth.gif",
+                  height: 150,
+                ),
+              ),
+              const Text(
+                "Terra Nova",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      autofocus: true,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        labelText: "E-mail",
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                        prefixIcon: Icon(
+                          Icons.mail_outline,
+                          color: Colors.white,
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextFormField(
+                      /*validator: (value) {
+                        if (value.length < 6) {
+                          return "A senha deve ter pelo menos 6 caracteres";
+                        }
+                        return null;
+                      },
+                      */
+                      //obscureText: _obscurePassword,
+                      //controller: _passwordInputController,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        labelText: "Senha",
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                        prefixIcon: Icon(
+                          //Icons.vpn_key_sharp,
+                          Icons.vpn_key,
+                          color: Colors.white,
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    )
+                  ], //children
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 8),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: const Text(
+                  "Esqueceu a senha?",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                  textAlign: TextAlign.right,
+                ),
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                    value: _isChecked,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        _isChecked = value!; // ainda a implementar
+                      });
+                    },
+                    activeColor: Colors.blueGrey,
+                  ),
+                  const Text(
+                    "Mostrar senha",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ], //children
+          ),
+        ),
+      ),
+    );
   }
 }
+
+/*
+body: Container(
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),//
+        
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                  bottom: 7,
+                ),
+                child: Image.asset(
+                  "assets/dumbbell.png",
+                  height: 125,
+                ),
+              ),
+              Text(
+                "Entrar",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (value) {
+                        if (value.length < 5) {
+                          return "Esse e-mail parece curto demais";
+                        } else if (!value.contains("@")) {
+                          return "Esse e-mail está meio estranho, não?";
+                        }
+                        return null;
+                      },
+                      controller: _mailInputController,
+                      autofocus: true,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        labelText: "E-mail",
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                        prefixIcon: Icon(
+                          Icons.mail_outline,
+                          color: Colors.white,
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value.length < 6) {
+                          return "A senha deve ter pelo menos 6 caracteres";
+                        }
+                        return null;
+                      },
+                      obscureText: _obscurePassword,
+                      controller: _passwordInputController,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        labelText: "Senha",
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                        prefixIcon: Icon(
+                          Icons.vpn_key_sharp,
+                          color: Colors.white,
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ), //form
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                  "Esqueceu a senha?",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                  textAlign: TextAlign.right,
+                ),
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                    value: this._obscurePassword,
+                    onChanged: (bool newValue) {
+                      setState(() {
+                        this._obscurePassword = newValue;
+                      });
+                    },
+                    activeColor: Colors.blue,
+                  ),
+                  Text(
+                    "Mostrar senha",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _doLogin();
+                },
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    CustomColors().getActivePrimaryButtonColor(),
+                  ),
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Divider(
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                "Ainda não tem uma conta?",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 11),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                // ignore: deprecated_member_use
+                child: RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SignUpPage(),
+                      ),
+                    );
+                  },
+                  child: Text("Cadastre-se"),
+                  color: CustomColors().getActiveSecondaryButton(),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    */
